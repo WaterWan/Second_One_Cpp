5ea154b58fde6b86dece9ec88909f437754ce202
Water <598127577@qq.com>
2016-08-13 16:44:52 +0800

    vs

---
+++ VS test/test/test/test.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+namespace A {
+	int x = 1;
+	void fun() {
+		cout << "A" << endl;
+	}
+}
+
+namespace B {
+	int x = 2;
+	void fun() {
+		cout << "B" << endl;
+	}
+
+	void fun2() {
+		cout << "2B" << endl;
+	}
+}
+
+using namespace B;
+
+int main() {
+	cout << A::x << endl;
+	B::fun();
+	fun2();
+
+	system("pause");
+	return 0;
+}
