dc8c25d9c12a870e08c929422517074d7a462f95
Water <598127577@qq.com>
2016-08-13 17:14:02 +0800

    vs

--- VS test/test/test/test.cpp
+++ VS test/test/test/test.cpp
@@ -1,33 +1,36 @@
 #include <iostream>
 #include <stdlib.h>
-
 using namespace std;
 
-namespace A {
-	int x = 1;
-	void fun() {
-		cout << "A" << endl;
-	}
-}
-
-namespace B {
-	int x = 2;
-	void fun() {
-		cout << "B" << endl;
-	}
-
-	void fun2() {
-		cout << "2B" << endl;
-	}
-}
-
-using namespace B;
+int getMaxOrMin(int *arr, int count, bool isMax);
 
 int main() {
-	cout << A::x << endl;
-	B::fun();
-	fun2();
-
+	int arr1[4] = { 3, 5, 1, 7 };
+	bool isMax = false;
+	cin >> isMax;
+	cout << getMaxOrMin(arr1, 4, isMax) << endl;
 	system("pause");
 	return 0;
 }
+
+int getMaxOrMin(int *arr, int count, bool isMax) {
+	int temp = arr[0];
+	if(isMax){
+		for (int i = 1; i < count; i++) {
+			if (temp < arr[i]) {
+				temp = arr[i];
+			}
+		}
+	} else {
+		for (int i = 1; i < count; i++) {
+			if (temp > arr[i]) {
+				temp = arr[i];
+			}
+		}
+	}
+
+	return temp;
+}
+
+
+
