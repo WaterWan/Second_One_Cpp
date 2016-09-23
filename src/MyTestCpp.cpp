ca447bc03860ab89c80423849f7a3478df2f22f0
WanZ <598127577@qq.com>
2016-09-23 16:50:06 +0800

    666

---
+++ src/MyTestCpp.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : MyTestCpp.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "666" << endl; // prints !!!Hello World!!!
+	return 0;
+}
